92e968439a44f0588f8fabab3538c5c813fcd4b2
ankit yadav <119887243+ankityadav8299@users.noreply.github.com>
2025-01-11 10:39:21 +0530

    Update client.py

--- jarvis/client.py
+++ jarvis/client.py
@@ -1,6 +1,6 @@
 import google.generativeai as genai
 
-genai.configure(api_key="AIzaSyASTjhJI80UJvowouC55vNED4uY7ksYNR8")
+genai.configure(api_key="your_api_key")
 model = genai.GenerativeModel("gemini-1.5-flash")
 response = model.generate_content("Explain how AI works")
 print(response.text)
