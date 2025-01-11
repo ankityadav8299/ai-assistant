d5f2ba9d84fffdc2a1ac4dbdb8182ee733ce37b9
ankit yadav <119887243+ankityadav8299@users.noreply.github.com>
2025-01-11 10:38:46 +0530

    Update musicLibrary.py

--- jarvis/musicLibrary.py
+++ jarvis/musicLibrary.py
@@ -3,6 +3,5 @@ music = {
     "march": "https://www.youtube.com/watch?v=Xqeq4b5u_Xw",
     "skyfall": "https://www.youtube.com/watch?v=DeumyOzKqgI&pp=ygUHc2t5ZmFsbA%3D%3D",
     "wolf": "https://www.youtube.com/watch?v=ThCH0U6aJpU&list=PLnrGi_-oOR6wm0Vi-1OsiLiV5ePSPs9oF&index=21",
-    "ishq": "https://www.youtube.com/watch?v=hHuG7FIKgtc&list=RDhHuG7FIKgtc&start_radio=1",
     "playlist": "https://www.youtube.com/watch?v=ilNt2bikxDI&list=PLJxkzg_Wet79g9pK1_1liLyuObXPB8RNT&pp=gAQBiAQB"
 }
